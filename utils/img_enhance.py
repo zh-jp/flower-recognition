0273b2efc2aef705d312e6ae75afdf3501b311d0
miku122 <hatsunemiku122321@gmail.com>
2023-03-07 21:23:43 +0800

    完成任务书与答辩PPT

--- utils/img_enhance.py
+++ utils/img_enhance.py
@@ -72,13 +72,13 @@ def imgHFlip(img):
 
 if __name__ == "__main__":
     # 读取图像
-    img = cv2.imread(r'C:\Users\hatsu\Desktop\images_for_test\1.jpg')
+    img = cv2.imread(r'C:\Users\hatsu\Desktop\images_for_test\4.jpg')
     # 显示图像
     cv2.imshow('Original', img)
     # cv2.imshow('Adjust', imgAdjust(img))
     # cv2.imshow('Rotate', imgRotate(img))
     # cv2.imshow('Translate', imgTranslate(img))
-    cv2.imshow('after', imgHFlip(img))
+    cv2.imshow('after', imgCrop(imgHFlip(img)))
     # cv2.imshow('Resize', imgResize(img))
     # cv2.imshow('Color', imgColor(img))
     cv2.waitKey(0)
